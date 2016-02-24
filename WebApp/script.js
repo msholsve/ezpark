8a59511ec2b68d17818cdc2fd4c6367d9f05c1a7
Martin Jensen <martin.jensen.91@gmail.com>
2016-02-24 12:39:02 +0100

    fixed automatic refreshing of room

--- WebApp/script.js
+++ WebApp/script.js
@@ -1,96 +1,125 @@
 $(document).ready(function() {
+    $.ajaxSetup({ cache: false });
     $("#lesesaler").hide();
     $("#info").hide();
     $("#roomImage").hide();
     $("#button").hide();
-    showRooms("http://isit.routable.org/rooms");
+    showRooms();
 
 });
 
-function showRooms(roomsJSON) {
+var roomsJSON = "http://isit.routable.org/rooms";
+
+function showRooms() {
     $.getJSON(roomsJSON, function(result) {
 	$("#info").fadeOut("slow");
 	$("#roomImage").fadeOut("slow");
 	$("#button").fadeOut("slow", function() {
-	    $("#roomImage").html("");
+	    clearSeatsOnImage();
 	    $("#info").fadeIn("slow");
 	    $("#lesesaler").fadeIn("slow");
 
 	    $("#info").html("Velkommen til iSit. Velg en lesesal for mer informasjon");
-	    var rooms = result._items;
-	    var table = "<thead>" +
-		"<tr>" +
-		"<th>Lesesaler</th><th>Ledige plasser</th>" + 
-		"</tr>" + 
-		"</thead>";
-
-	    for (var i = 0; i < rooms.length; i++) {
-		table += "<tr>" +
-		    "<td id=" + rooms[i]._id + ">" +
-		    rooms[i].name +
-		    "</td><td>" + String(rooms[i].free_seats) + " / " + String(rooms[i].total_seats) +
-		    "</td>" +
-		    "</tr>";
-	    }
-
-	    $("#lesesaler").html(table);
-
-	    for (i = 0; i < rooms.length; i++) {
-		bindListItemToRoom(rooms[i]._id, roomsJSON + "/" + rooms[i].name);
-	    }
+	    fillTableWithRooms(result._items);
 	});
     });
 }
 
-function bindListItemToRoom(roomId, roomJSON) {
-    $("#" + roomId).click(function () {
-	    showRoom(roomJSON);
+function fillTableWithRooms(rooms) {
+    var table = "<thead>" +
+	"<tr>" +
+	"<th>Lesesaler</th><th>Ledige plasser</th>" + 
+	"</tr>" + 
+	"</thead>";
+
+    for (var i = 0; i < rooms.length; i++) {
+	table += "<tr>" +
+	    "<td id=" + rooms[i]._id + ">" +
+	    rooms[i].name +
+	    "</td><td>" + String(rooms[i].free_seats) + " / " + String(rooms[i].total_seats) +
+	    "</td>" +
+	    "</tr>";
+    }
+
+    $("#lesesaler").html(table);
+
+    for (i = 0; i < rooms.length; i++) {
+	bindListItemToRoom(rooms[i]._id, rooms[i].name);
+    }
+}
+
+function bindListItemToRoom(id, name) {
+    $("#" + id).click(function () {
+	    showRoom(name);
     });
 }
 
-function showRoom(roomJSON) {
-    $.getJSON(roomJSON, function(result) {
+function showRoom(name) {
+    $.getJSON(roomsJSON + "/" + name, function(roomWithImage) {
 	$("#lesesaler").fadeOut("slow");
 	$("#info").fadeOut("slow", function() {
 	    $("#button").fadeIn("slow");
 	    $("#roomImage").fadeIn("slow");
 	    $("#info").fadeIn("slow");
 
-
-	    $("#info").html("Du valgte " + result.name);
+	    $("#info").html("Du valgte " + roomWithImage.name);
 
 	    var img = new Image();
-	    img.src = "data:image/png;base64," + result.map.file;
-	    $("#roomImage").css({
-		"height":img.height + "px",
-		"width":img.width + "px",
-		"background-image":
-		"url('" + img.src + "')"
-		});
-
-	    
-	    for (i = 0; i < result.seats.length; i++) {
-		var free = "#00FF00";
-		if (result.seats[i].free == false) {
-		    free = "#FF0000";
-		}
-
-		$("#roomImage").append(
-		    $('<div><div>').css({
-			position: 'relative',
-			top: result.seats[i].location.y + "px",
-			left: result.seats[i].location.x + "px",
-			width: "10px",
-			height: "10px",
-			background: free
-		    })
-		);
+	    img.src = "data:image/png;base64," + roomWithImage.map.file;
+	    img.onload = function () {
+		setImageAsBackground(img);
+		placeSeatsOnImage(roomWithImage.seats);
 	    }
+
+	    var refresh = setInterval( function () {
+		$.getJSON(
+		    roomsJSON + "/" + name + "?projection={\"map\":%200}",
+		    function (roomWithoutImage) {
+			console.log(roomWithoutImage.seats[0]);
+			console.log(roomWithoutImage.seats[1]);
+			clearSeatsOnImage();
+			placeSeatsOnImage(roomWithoutImage.seats);
+			
+		    })
+	    }, 1000);
 
 	    $("#button").html("Tilbake");
 	    $("#button").click( function () {
-		showRooms("http://isit.routable.org/rooms");
+		clearInterval(refresh);
+		showRooms(roomsJSON);
 	    });
 	});
     });
+}
+
+function setImageAsBackground(image) {
+    $("#roomImage").css({
+	"height":image.naturalHeight + "px",
+	"width":image.naturalWidth + "px",
+	"background-image":"url('" + image.src + "')"
+	});
+}
+
+function placeSeatsOnImage(seats) {
+    for (i = 0; i < seats.length; i++) {
+	var free = "#00FF00";
+	if (seats[i].free == false) {
+	    free = "#FF0000";
+	}
+	
+	$("#roomImage").append(
+	    $('<div><div>').css({
+		position: 'relative',
+		top: seats[i].location.y + "px",
+		left: seats[i].location.x + "px",
+		width: "10px",
+		height: "10px",
+		background: free
+	    })
+	);
+    }
+}
+
+function clearSeatsOnImage() {
+    $("#roomImage").html("");
 }
