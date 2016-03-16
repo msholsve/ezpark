c2215232f4ea08067530be1053c8645d0ef5fab0
Martin Jensen <martin.jensen.91@gmail.com>
2016-03-16 21:22:51 +0100

    Updated webpage to include map view

--- WebApp/script.js
+++ WebApp/script.js
@@ -1,124 +1,199 @@
-$(document).ready(function() {
+var roomsJSON = 'http://isit.routable.org/api/rooms';
+var currentView;
+var selectedTab;
+var refresh;
+
+const LISTVIEW = 1;
+const MAPVIEW = 2;
+const DETAILEDVIEW = 3;
+
+$( document ).ready( function() {
+    
     $.ajaxSetup({ cache: false });
-    $("#lesesaler").hide();
-    $("#info").hide();
-    $("#roomImage").hide();
-    $("#button").hide();
-    showRooms();
 
-});
+    $( '#info' ).html( 'Velkommen til Isit!' );
+    $( '#map' ).html( 'Her skal kartet være' );
 
-var roomsJSON = "http://isit.routable.org/api/rooms";
+    $( '#backButton' ).hide();
+    $( '#list' ).hide();
+    $( '#map' ).hide();
+    $( '#roomImage' ).hide();
 
-function showRooms() {
-    $.getJSON(roomsJSON, function(result) {
-	$("#info").fadeOut("slow");
-	$("#roomImage").fadeOut("slow");
-	$("#button").fadeOut("slow", function() {
-	    clearSeatsOnImage();
-	    $("#info").fadeIn("slow");
-	    $("#lesesaler").fadeIn("slow");
-
-	    $("#info").html("Velkommen til iSit. Velg en lesesal for mer informasjon");
-	    fillTableWithRooms(result._items);
-	});
+    $( '#listViewTab' ).click( function() {
+	selectedTab = LISTVIEW;
+	changeView( LISTVIEW, {} );
     });
+
+    $( '#mapViewTab' ).click( function() {
+	selectedTab = MAPVIEW;
+	changeView( MAPVIEW, {} );
+    });
+
+    currentView = LISTVIEW;
+    selectedTab = LISTVIEW;
+    transitionIn( currentView, {} );
+} );
+
+function changeView( view, params ) {
+    transitionOut( transitionIn, view, params );
+    currentView = view;
 }
 
-function fillTableWithRooms(rooms) {
-    var table = "<thead>" +
-	"<tr>" +
-	"<th>Lesesaler</th><th>Ledige plasser</th>" + 
-	"</tr>" + 
-	"</thead>";
 
-    for (var i = 0; i < rooms.length; i++) {
-	table += "<tr>" +
-	    "<td id=" + rooms[i]._id + ">" +
-	    rooms[i].name +
-	    "</td><td>" + String(rooms[i].free_seats) + " / " + String(rooms[i].total_seats) +
-	    "</td>" +
-	    "</tr>";
+function transitionOut( transitionIn, view, params ) {
+    var selector;
+
+    console.log( 'transition from', currentView, 'to', view );
+
+    switch( currentView ) {
+    case LISTVIEW:
+	selector = '.list-view';
+	break;
+
+    case MAPVIEW:
+	selector = '.map-view';
+	break;
+
+    case DETAILEDVIEW:
+	clearInterval( refresh );
+	selector = '.detailed-view';
+	break;
+
+    default:
+	console.error( 'view is invalid' );
     }
 
-    $("#lesesaler").html(table);
+    $( selector ).fadeOut( 'fast' );
+    $( selector ).promise().done( function() {
+	transitionIn( view, params );
+    } );
+}
 
-    for (i = 0; i < rooms.length; i++) {
-	bindListItemToRoom(rooms[i]._id, rooms[i].name);
+
+function transitionIn( view, params ) {
+    if ( view == LISTVIEW ) {
+	showListView().done( function() {
+	    $( '.list-view' ).fadeIn( 'fast' );
+	} );
+
+    } else if ( view == MAPVIEW ) {
+	$( '.map-view' ).fadeIn( 'fast' );
+	showMapView();
+
+    } else if ( view == DETAILEDVIEW ) {
+	showDetailedView( params.id, params.name ).done( function() {
+	    $( '.detailed-view' ).fadeIn( 'fast' );
+	} );
     }
 }
 
-function bindListItemToRoom(id, name) {
-    $("#" + id).click(function () {
-	showRoom(id, name);
+
+function showListView() {
+    return $.getJSON( roomsJSON, function( result ) {
+	$( '#info' ).html( 'Velg en lesesal fra listen for mer informasjon' );
+	fillListWithRooms( result._items );
     });
 }
 
-function showRoom(id, name) {
-    $.getJSON(roomsJSON + "/" + id, function(roomWithImage) {
-	$("#lesesaler").fadeOut("slow");
-	$("#info").fadeOut("slow", function() {
-	    $("#button").fadeIn("slow");
-	    $("#roomImage").fadeIn("slow");
-	    $("#info").fadeIn("slow");
 
-	    $("#info").html("Du valgte " + roomWithImage.name);
-
-	    var img = new Image();
-	    img.src = "data:image/png;base64," + roomWithImage.map.file;
-	    img.onload = function () {
-		setImageAsBackground(img);
-		placeSeatsOnImage(roomWithImage.seats);
-	    }
-
-	    var refresh = setInterval( function () {
-		$.getJSON(
-		    roomsJSON + "/" + id + "?projection={\"map\":%200}",
-		    function (roomWithoutImage) {
-			clearSeatsOnImage();
-			placeSeatsOnImage(roomWithoutImage.seats);
-			
-		    })
-	    }, 1000);
-
-	    $("#button").html("Tilbake");
-	    $("#button").click( function () {
-		clearInterval(refresh);
-		showRooms(roomsJSON);
-	    });
-	});
-    });
+function showMapView() {
 }
 
-function setImageAsBackground(image) {
-    $("#roomImage").css({
-	"height":image.naturalHeight + "px",
-	"width":image.naturalWidth + "px",
-	"background-image":"url('" + image.src + "')"
-	});
-}
 
-function placeSeatsOnImage(seats) {
-    var free;
-    for (i = 0; i < seats.length; i++) {
-	free = "#00FF00";
-	if (seats[i].free == false) {
-	    free = "#FF0000";
+function showDetailedView( id, name ) {
+    return $.getJSON( roomsJSON + '/' + id, function( roomWithImage ) { 
+	
+	$( '#info' ).html( 'Du valgte ' + roomWithImage.name );
+	$( '#backButton' ).html( 'Tilbake' );
+	clearSeatsOnImage();
+	
+	var img = new Image();
+	img.src = 'data:image/png;base64,' + roomWithImage.map.file;
+	img.onload = function () {
+	    setImageAsBackground( img );
+	    placeSeatsOnImage( roomWithImage.seats );
 	}
 	
-	$("#roomImage").append(
-	    $('<div><div>').css({
+	refresh = setInterval( function () {
+	    $.getJSON(
+		roomsJSON + '/' + id + '?projection={"map":%200}',
+		function ( roomWithoutImage ) {
+		    clearSeatsOnImage();
+		    placeSeatsOnImage( roomWithoutImage.seats );
+		    
+		})
+	}, 1000);
+	
+	$( '#backButton' ).unbind();
+	$( '#backButton' ).click( function () {
+	    changeView( selectedTab );
+	} );
+    } );
+}
+
+
+function fillListWithRooms( rooms ) {
+    var table = '<thead>' +
+	'<tr>' +
+	'<th>Lesesaler</th><th>Ledige plasser</th>' + 
+	'</tr>' + 
+	'</thead>';
+
+    for ( var i = 0; i < rooms.length; i++ ) {
+	table += '<tr>' +
+	    '<td id=' + rooms[i]._id + '>' +
+	    rooms[i].name +
+	    '</td><td>' + String(rooms[i].free_seats) + ' / ' + String(rooms[i].total_seats) +
+	    '</td>' +
+	    '</tr>';
+    }
+
+    $( '#list' ).html( table );
+
+    for ( i = 0; i < rooms.length; i++ ) {
+	bindListItemToRoom( rooms[i]._id, rooms[i].name );
+    }
+}
+
+
+function bindListItemToRoom( id, name ) {
+    $( '#' + id ).click( function () {
+	changeView( DETAILEDVIEW, {id: id, name: name} );
+    } );
+}
+
+
+function setImageAsBackground( image ) {
+    $( '#roomImage' ).css({
+	'height':image.naturalHeight + 'px',
+	'width':image.naturalWidth + 'px',
+	'background-image':"url('" + image.src + "')"
+	});
+}
+
+
+function placeSeatsOnImage( seats ) {
+    var free;
+    for ( i = 0; i < seats.length; i++ ) {
+	free = '#00FF00';
+	if ( seats[i].free == false ) {
+	    free = '#FF0000';
+	}
+	
+	$( '#roomImage' ).append(
+	    $( '<div><div>' ).css({
 		position: 'relative',
-		top: seats[i].location.y + "px",
-		left: seats[i].location.x + "px",
-		width: "10px",
-		height: "10px",
+		top: seats[i].location.y + 'px',
+		left: seats[i].location.x + 'px',
+		width: '10px',
+		height: '10px',
 		background: free
 	    })
 	);
     }
 }
 
+
 function clearSeatsOnImage() {
-    $("#roomImage").html("");
+    $( '#roomImage' ).html( '' );
 }
