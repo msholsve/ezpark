f4b15a667de268f16e52d3223ce8123410fc87cd
Martin Jensen <martin.jensen.91@gmail.com>
2016-03-16 21:29:10 +0100

    fixed merge conflict

--- WebApp/script.js
+++ WebApp/script.js
@@ -99,8 +99,6 @@ function showListView() {
 function showMapView() {
 }
 
-<<<<<<< HEAD
-
 function showDetailedView( id, name ) {
     return $.getJSON( roomsJSON + '/' + id, function( roomWithImage ) { 
 	
@@ -169,14 +167,6 @@ function setImageAsBackground( image ) {
 	'height':image.naturalHeight + 'px',
 	'width':image.naturalWidth + 'px',
 	'background-image':"url('" + image.src + "')"
-=======
-function setImageAsBackground(image) {
-    $("#roomImage").css({
-        "position":'relative',
-	"height":image.naturalHeight + "px",
-	"width":image.naturalWidth + "px",
-	"background-image":"url('" + image.src + "')"
->>>>>>> 5c49aa15ec3bd29032edbb5428bf725f799eae28
 	});
 }
 
@@ -189,7 +179,6 @@ function placeSeatsOnImage( seats ) {
 	    free = '#FF0000';
 	}
 	
-<<<<<<< HEAD
 	$( '#roomImage' ).append(
 	    $( '<div><div>' ).css({
 		position: 'relative',
@@ -197,15 +186,6 @@ function placeSeatsOnImage( seats ) {
 		left: seats[i].location.x + 'px',
 		width: '10px',
 		height: '10px',
-=======
-	$("#roomImage").append(
-	    $('<div><div>').css({
-		position: 'absolute',
-		top: seats[i].location.y + "px",
-		left: seats[i].location.x + "px",
-		width: "10px",
-		height: "10px",
->>>>>>> 5c49aa15ec3bd29032edbb5428bf725f799eae28
 		background: free
 	    })
 	);
